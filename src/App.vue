9c29e1fd1583e90a58a09efae9ef9bafdb0a96ed
alefemacedo <almacedobr2@gmail.com>
2024-11-14 18:42:21 -0300

    [master] - Implementação da tela de "Em construção"

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,17 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+    <ui-in-construction />
 </template>
+
+<script setup>
+    import InConstruction from '@/components/InContruction';
+    
+    defineOptions({
+        components: {
+            'ui-in-construction': InConstruction
+        }
+    })
+</script>
 
 <style scoped>
 header {
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,13 +7,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
     },
-  },
+    css: {
+        preprocessorOptions: {
+            stylus: {
+                additionalData: `@import "${path.resolve(__dirname, 'src/assets/styles/colors.styl')}"`,
+            },
+        },
+    },
 })
